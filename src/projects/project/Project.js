648c8daf5a18d7e7a1ae536798543d4acb0c3f54
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-15 00:41:03 +0300

    Added and styles Project component

---
+++ src/projects/project/Project.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import style from './Project.module.css'
+
+function Project (props) {
+   return (
+      <div className={style.project}>
+         <div className={style.photo}>
+            <button className={style.projectButton}>View</button>
+         </div>
+         <div className={style.descriptionContainer}>
+         <h3>{props.title}</h3>
+         <span>{props.description}</span>
+         </div>
+      </div>
+   )
+}
+
+export default Project

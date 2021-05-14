6854a443b8173d27c252bec9915891bf9976ef4a
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-15 00:45:39 +0300

    Added all components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import './App.css';
+import Contacts from './contacts/Contacts';
+import Footer from './footer/Footer';
 import Header from './header/Header';
+import Job from './job/Job';
 import Main from './main/Main';
+import Projects from './projects/Projects';
 import Skills from './skills/Skills';
 
 function App() {
@@ -10,6 +14,10 @@ function App() {
       <Header />
       <Main />
       <Skills />
+      <Projects/>
+      <Job/>
+      <Contacts />
+      <Footer />
     </div>
   );
 }

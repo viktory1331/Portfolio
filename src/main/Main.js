64424e6dd9047f3ae827bb13e84bd98fd19bde87
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-14 20:30:38 +0300

    Added and styled Main component

---
+++ src/main/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import style from './Main.module.css';
+import styleContainer from '../common/styles/Container.module.css'
+
+function Main () {
+   return (
+      <div className={style.mainBlock} >
+         <div className={styleContainer.container}>
+         <div className={style.greeting}>
+            <h1>Fedosowa Victoria</h1>
+            <h2>I'm front-end developer</h2>
+         </div>
+         <div className={style.photo}></div>
+         </div>
+      </div>
+   )
+}
+
+
+export default Main

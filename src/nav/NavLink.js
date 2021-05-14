92c36c251a6b86c15251e2a0e8f6c3de103f50b4
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-14 20:36:41 +0300

    Added and styles NavLink component

---
+++ src/nav/NavLink.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import style from './NavLink.module.css'
+
+function NavLink () {
+   return (
+      <div className={style.navLink}>
+         <a href="">Main</a> 
+         <a href="">Skills</a> 
+         <a href="">Projects</a> 
+         <a href="">Contacts</a> 
+      </div>
+   )
+}
+
+export default NavLink

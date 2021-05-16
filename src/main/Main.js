e76971bf2198d33496f432053c713b349e898df4
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-16 22:38:01 +0300

    Update Main style

--- src/main/Main.js
+++ src/main/Main.js
@@ -5,7 +5,7 @@ import styleContainer from '../common/styles/Container.module.css'
 function Main () {
    return (
       <div className={style.mainBlock} >
-         <div className={styleContainer.container}>
+         <div className={`${styleContainer.container} ${style.mainContainer}`}>
          <div className={style.greeting}>
             <h1>Fedosowa Victoria</h1>
             <h2>I'm front-end developer</h2>

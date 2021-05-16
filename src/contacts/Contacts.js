374c216c07b388d0ca6b70b94059e43946c43b85
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-16 22:29:30 +0300

    Update Contacts style

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -8,7 +8,7 @@ function Contacts () {
          <div className={`${styleContainer.container} ${style.contactsContainer}`}>
             <h2 className={style.title}>Contacts</h2>       
             <form className={style.form} method={'post'}>
-               <input className={style.inputElement} type={'text'} name={'fname'} placeholder={'Name'} class={'form-control'}  required={''} />
+               <input className={style.inputElement} type={'text'} name={'fname'} placeholder={'Name'}  />
                <input className={style.inputElement} type={'text'} name={'fname'} placeholder={'Email'}  />
                <textarea className={style.textarea} placeholder={'Message'} />
             </form>

7ed71bfec2a70f8b9e27a14a47ee3eabf27aadc6
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-29 21:35:23 +0300

    Added animation with help useState

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import style from './Project.module.css';
+import React, { useState } from 'react';
+import style from './Project.module.scss';
 
 function Project(props) {
+  const [isHover, setIsHover] = useState(false);
+
   return (
     <div className={style.project}>
-      <div className={style.projectPhotoContainer} style={props.style}>
-        <button className={style.projectButton} >View</button>
+      <div
+        className={`${style.projectPhotoContainer} ${
+          isHover ? style.isHover : ''
+        }`}
+        style={props.style}
+      >
+        <button
+          className={style.projectButton}
+          onMouseEnter={() => setIsHover(true)}
+          onMouseLeave={() => setIsHover(false)}
+        >
+          View
+        </button>
       </div>
     </div>
   );

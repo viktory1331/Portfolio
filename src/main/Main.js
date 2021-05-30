1603bc91c416d1b3dec13174f87be82c834576fe
viktory1331 <viktoriyafedosowa@gmail.com>
2021-05-30 23:01:21 +0300

    Updates Main structure

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import style from './Main.module.css';
-import styleContainer from '../common/styles/Container.module.css'
+import React from 'react';
+import style from './Main.module.scss';
+import styleContainer from '../common/styles/Container.module.css';
 
-function Main () {
-   return (
-      <div className={style.mainBlock} >
-         <div className={`${styleContainer.container} ${style.mainContainer}`}>
-         <div className={style.greeting}>
-            <h1>Fedosowa Victoria</h1>
-            <h2>I'm front-end developer</h2>
-         </div>
-         <div className={style.photo}></div>
-         </div>
+function Main() {
+  return (
+    <div className={style.mainBlock}>
+      <div className={`${styleContainer.container} ${style.mainContainer}`}>
+        <div className={style.photo}></div>
+        <div className={style.greeting}>
+          <h1 className={style.myName}>Fedosowa Victoria</h1>
+          <h2 className={style.mySpecialization}>I'm front-end developer</h2>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
 
-
-export default Main
+export default Main;

f9951c788e1dfad1db281440c24a7a95016ce044
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 01:05:15 +0300

    Update some classNames and added styles

--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import style from './Skill.module.css';
+import React from 'react';
+import style from './Skill.module.scss';
 
-
-function Skill (props) {
-   return (
-      <div className={style.skill} >
-         <div className={style.icon}></div>
-         <h3>{props.title}</h3>
-         <span className={style.description}>{props.description}</span>
-      </div>
-   )
+function Skill(props) {
+  return (
+    <div className={style.skill}>
+      <div className={style.icon}></div>
+      <h3 className={style.title}>{props.title}</h3>
+      <span className={style.description}>{props.description}</span>
+    </div>
+  );
 }
 
-
-export default Skill
+export default Skill;

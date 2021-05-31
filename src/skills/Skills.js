d3361787c9383783e4dd154ace47a0c94a2b9b0f
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 01:03:58 +0300

    Update content and styles

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,23 +1,42 @@
-import React from 'react'
-import style from './Skills.module.css';
-import styleContainer from '../common/styles/Container.module.css'
+import React from 'react';
+import style from './Skills.module.scss';
+import styleContainer from '../common/styles/Container.module.css';
 import Skill from './skill/Skill';
 
-
-function Skills () {
-   return (
-      <div className={style.skillsBlock} >
-         <div className={`${styleContainer.container} ${style.skillsContainer}`}>
-            <h2 className={style.title}>MY SKILLS</h2>
-            <div className={style.skills}>
-            <Skill title={'React/Redux'} description={'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'} />
-            <Skill title={'JS'} description={'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'}/>
-            <Skill title ={'HTML&CSS'} description={'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'}/>
-            </div>
-         </div>
+function Skills() {
+  return (
+    <div className={style.skillsBlock}>
+      <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+        <h2 className={style.title}>MY SKILLS</h2>
+        <div className={style.skills}>
+          <Skill
+            title={'React / Redux / Thunk'}
+            description={
+              'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'
+            }
+          />
+          <Skill
+            title={'JS(ES6+) / TypeScript'}
+            description={
+              'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'
+            }
+          />
+          <Skill
+            title={'HTML / CSS / SASS'}
+            description={
+              'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'
+            }
+          />
+          <Skill
+            title={'TDD / Storybook / GIT'}
+            description={
+              'Fusce suscipit, ante a hendrerit thelery ullamcorper, risus nisl cursus purus the viverra ante nulla vel justo.'
+            }
+          />
+        </div>
       </div>
-   )
+    </div>
+  );
 }
 
-
-export default Skills
+export default Skills;

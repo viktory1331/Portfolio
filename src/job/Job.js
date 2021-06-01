1c6bff0b2b64f514ca4e6a1cdcf8c9b7f3725746
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 22:28:54 +0300

    Update Job blocks

--- src/job/Job.js
+++ src/job/Job.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
 import style from './Job.module.scss';
-import styleContainer from '../common/styles/Container.module.css'
+import styleContainer from '../common/styles/Container.module.css';
 
-function Job () {
-   return (
-      <div className={style.jobBlock} >
-         <div className={`${styleContainer.container} ${style.jobContainer}`}>
-         <h2 className={style.title}>I am considering remote work options</h2>
-         <button className={style.jobButton}>Hire me</button>
-         </div>
+function Job() {
+  return (
+    <div className={style.jobBlock}>
+      <div className={`${styleContainer.container} ${style.jobContainer}`}>
+        <h2 className={style.title}>I am considering remote work options</h2>
+        <div className={style.buttonsContainer}>
+          <button className={style.jobButton}>Hire me</button>
+          <button className={style.jobButton}>Download CV</button>
+        </div>
       </div>
-   )
+    </div>
+  );
 }
 
-
-export default Job
+export default Job;

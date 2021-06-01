4fee86da0d215838cfabb973c8c29a99e026522c
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-01 22:27:33 +0300

    Update contacts block

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -1,22 +1,33 @@
-import React from 'react'
-import style from './Contacts.module.css';
-import styleContainer from '../common/styles/Container.module.css'
+import React from 'react';
+import style from './Contacts.module.scss';
+import styleContainer from '../common/styles/Container.module.css';
 
-function Contacts () {
-   return (
-      <div className={style.contactsBlock} >
-         <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-            <h2 className={style.title}>Contacts</h2>       
-            <form className={style.form} method={'post'}>
-               <input className={style.inputElement} type={'text'} name={'fname'} placeholder={'Name'}  />
-               <input className={style.inputElement} type={'text'} name={'fname'} placeholder={'Email'}  />
-               <textarea className={style.textarea} placeholder={'Message'} />
-            </form>
-            <button className={style.contactsButton}>Send</button>
-         </div>
+function Contacts() {
+  return (
+    <div className={style.contactsBlock}>
+      <div className={`${styleContainer.container} ${style.contactsContainer}`}>
+        <h2 className={style.title}>Get in touch</h2>
+        <form className={style.form} method={'post'}>
+          <div className={style.inputContainer}>
+            <input
+              className={style.inputElement1}
+              type={'text'}
+              name={'fname'}
+              placeholder={'Name'}
+            />
+            <input
+              className={style.inputElement2}
+              type={'text'}
+              name={'fname'}
+              placeholder={'Email'}
+            />
+          </div>
+          <textarea className={style.textarea} placeholder={'Message'} />
+        </form>
+        <button className={style.contactsButton}>Say Hello!</button>
       </div>
-   )
+    </div>
+  );
 }
 
-
-export default Contacts
+export default Contacts;

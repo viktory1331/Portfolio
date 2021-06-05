5046915d9313c0decc28fd59c0f54485d030dfdd
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-05 15:14:50 +0300

    Using other background and me profile photo

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import sunnyImg from '../assets/image/sunnyImg.jpg';
 import background from '../assets/image/background.jpg';
+
 
 function Main(props) {
   const backgroundImg = {
     backgroundImage: `url(${background})`,
   };
 
+
+  const myPhoto = {
+    backgroundImage: `url(${sunnyImg})`,
+  };
+
   return (
     <div className={style.mainBlock} style={backgroundImg}>
       <div className={`${styleContainer.container} ${style.mainContainer}`}>
-        <div className={style.photo}></div>
+        <div className={style.photo} style={myPhoto}></div>
         <div className={style.greeting}>
           <h1 className={style.myName}>Fedosowa Victoria</h1>
           <h2 className={style.mySpecialization}>I'm front-end developer</h2>

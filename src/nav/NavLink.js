2ea2a3860be774f12bdc854070ef648cd48d131b
viktory1331 <viktoriyafedosowa@gmail.com>
2021-06-11 18:04:05 +0300

    Using react-scroll

--- src/nav/NavLink.js
+++ src/nav/NavLink.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import style from './NavLink.module.scss'
+import React from 'react';
+import style from './NavLink.module.scss';
+import { Link, animateScroll as scroll } from 'react-scroll';
 
-function NavLink () {
-   return (
-      <div className={style.navLink}>
-         <a href="">Main</a> 
-         <a href="">Skills</a> 
-         <a href="">Projects</a> 
-         <a href="">Contacts</a> 
-      </div>
-   )
+function NavLink() {
+  return (
+    <div className={style.navLink}>
+      <Link
+        activeClass={style.active}
+        to="Main"
+        spy={true}
+        smooth={true}
+        offset={-80}
+        duration={500}
+      >
+        Main
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="Skills"
+        spy={true}
+        smooth={true}
+        offset={-80}
+        duration={500}
+      >
+        Skills
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="Projects"
+        spy={true}
+        smooth={true}
+        offset={-80}
+        duration={500}
+      >
+        Projects
+      </Link>
+      <Link
+        activeClass={style.active}
+        to="Contacts"
+        spy={true}
+        smooth={true}
+        offset={-80}
+        duration={500}
+      >
+        Contacts
+      </Link>
+    </div>
+  );
 }
 
-export default NavLink
+export default NavLink;
